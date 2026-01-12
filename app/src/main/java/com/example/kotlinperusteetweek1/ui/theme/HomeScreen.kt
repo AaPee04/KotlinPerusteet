c72999fff74d3b79e122c76b43853d4858c8e154
AaPee04 <arttupekka.konttila@gmail.com>
2026-01-12 19:29:13 +0200

    More work

--- app/src/main/java/com/example/kotlinperusteetweek1/ui/theme/HomeScreen.kt
+++ app/src/main/java/com/example/kotlinperusteetweek1/ui/theme/HomeScreen.kt
@@ -1,6 +1,8 @@
 package com.example.kotlinperusteetweek1.ui.theme
 
+import java.time.LocalDate
 import androidx.compose.foundation.layout.*
+import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -8,11 +10,16 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.kotlinperusteetweek1.domain.Task
 import com.example.kotlinperusteetweek1.domain.mockTasks
+import androidx.compose.foundation.clickable
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
 
 @Composable
 fun HomeScreen(
-    tasks: List<Task> = mockTasks
+    initialTasks: List<Task> = mockTasks
 ) {
+    var tasks by remember { mutableStateOf(initialTasks) }
+
     Column(
         modifier = Modifier
             .fillMaxSize()
@@ -20,21 +27,60 @@ fun HomeScreen(
     ) {
 
         Text(
-            text = "My Tasks",
+            text = "Tehtävät",
             fontSize = 24.sp,
             modifier = Modifier.padding(bottom = 16.dp)
         )
+
+        // NAPIT
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(bottom = 16.dp),
+            horizontalArrangement = Arrangement.SpaceBetween
+        ) {
+
+            Button(onClick = {
+                tasks = addTask(
+                    tasks,
+                    )
+
+            }) {
+                Text("Lisää")
+            }
+
+            Button(onClick = {
+                tasks = filterByDone(tasks, false)
+            }) {
+                Text("Avoimet")
+            }
+
+            Button(onClick = {
+                tasks = filterByDone(tasks, true)
+            }) {
+                Text("Tehdyt")
+            }
+
+            Button(onClick = {
+                tasks = sortByDueDate(tasks)
+            }) {
+                Text("Järjestä")
+            }
+        }
 
         tasks.forEach { task ->
             Row(
                 modifier = Modifier
                     .fillMaxWidth()
                     .padding(vertical = 8.dp)
+                    .clickable {
+                        tasks = toggleDone(tasks, task.id)
+                    }
             ) {
                 Column {
                     Text(text = task.title)
                     Text(
-                        text = if (task.done) "Done" else "Not done",
+                        text = if (task.done) "Tehty" else "Tehtävä",
                         fontSize = 12.sp
                     )
                 }
